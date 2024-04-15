e9ec90142694029b5348e5c31494d1fc2ecd09b9
ppcvaladao <100394501+ppcvaladao@users.noreply.github.com>
2024-04-15 00:57:12 -0300

    limpando app.js

--- app.js
+++ app.js
@@ -1,56 +1,42 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
-// Set 'views' directory for any views 
-// being rendered res.render()
-app.set('views', path.join(__dirname, 'views'));
 
-// Set view engine as EJS
+// Set up middleware to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set the directory for views and the view engine to EJS
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Rota principal that renders the EJS template
+// Main route to render the EJS template
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-// Simple in-memory storage for votes
+// In-memory storage for presidential votes
 let votes = {
-  vereador: { A: 0, B: 0 },
-  prefeito: { X: 0, Y: 0 }
+  '13': 0, // Candidate number for the first presidential candidate
+  '17': 0  // Candidate number for the second presidential candidate
 };
 
-
-// Rota para registrar votos para vereador
-app.get('/votar-vereador/:candidato', (req, res) => {
+// Route to register votes for presidential candidates
+app.get('/votar/:candidato', (req, res) => {
   const candidato = req.params.candidato;
-  if (votes.vereador.hasOwnProperty(candidato)) {
-    votes.vereador[candidato] += 1;
-    res.send(`Voto registrado para Vereador: ${candidato}`);
+  if (votes.hasOwnProperty(candidato)) {
+    votes[candidato] += 1;
+    res.send(`Voto registrado para o candidato número: ${candidato}`);
   } else {
-    res.status(400).send('Candidato inválido para vereador');
+    res.status(400).send('Candidato inválido para presidente');
   }
 });
 
-// Rota para registrar votos para prefeito
-app.get('/votar-prefeito/:candidato', (req, res) => {
-  const candidato = req.params.candidato;
-  if (votes.prefeito.hasOwnProperty(candidato)) {
-    votes.prefeito[candidato] += 1;
-    res.send(`Voto registrado para Prefeito: ${candidato}`);
-  } else {
-    res.status(400).send('Candidato inválido para prefeito');
-  }
-});
-
-// Rota para obter os votos totais
+// Route to retrieve total votes
 app.get('/resultados', (req, res) => {
-    res.json(votes);
-  });
-  
+  res.json(votes);
+});
 
+// Setting up the server to listen on port 3000
 const port = 3000;
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
